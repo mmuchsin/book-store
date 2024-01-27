acdcc25b063d155549d91322a2be1a296ae09740
mmuchsin <kangucin21@gmail.com>
2024-01-27 15:04:32 +0700

    allauth setup

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     # third patdy apps
     'crispy_forms',
     'crispy_bootstrap5',
