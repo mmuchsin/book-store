5b636824116a2dcf05152c53fd5b5f70bbabc128
mmuchsin <kangucin21@gmail.com>
2024-01-27 07:53:12 +0700

    add login, logou. signup and all tested

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse, resolve
+
+from .views import SignUpView
+from .forms import CustomUserCreationForm
 
 class CustomUserTestCase(TestCase):
     def test_create_user(self):
@@ -29,3 +33,30 @@ class CustomUserTestCase(TestCase):
         self.assertTrue(user.is_active)
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+
+
+class SignUpTestCase(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_signup_template_name(self):
+        self.assertTemplateUsed(self.response,'registration/signup.html')
+
+    def test_signup_content(self):
+        self.assertContains(self.response, 'Sign Up Page')
+
+    def test_signup_invalid_content(self):
+        self.assertNotContains(self.response, 'ngawurr coyyy')
+
+    def test_signup_view(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(view.func.__name__, SignUpView.as_view().__name__)
+
+    def test_signup_form(self):
+        form = self.response.context.get('form', 'invalid key: form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertTrue(self.response, 'csrfmiddlewatetoken')
